0bb95536d7e39153d65391f802c5eca84c0079e6
Alvi <huskartp@gmail.com>
2022-08-31 11:19:41 +0300

    Trying to configure kafka

--- src/main/java/ink/twoa/webserver/WebserverApplication.java
+++ src/main/java/ink/twoa/webserver/WebserverApplication.java
@@ -2,7 +2,9 @@ package ink.twoa.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class WebserverApplication {
 
---
+++ src/main/java/ink/twoa/webserver/service/KafkaService.java
@@ -0,0 +1,42 @@
+package ink.twoa.webserver.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ink.twoa.webserver.model.entity.UsersEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaService {
+
+    private final KafkaTemplate<Long, UsersEntity> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public KafkaService(KafkaTemplate<Long, UsersEntity> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void send(UsersEntity users){
+        kafkaTemplate.send("msg", users);
+    }
+
+    @KafkaListener(id = "Users", topics = {"msg"}, containerFactory = "singleFactory")
+    public void consume(UsersEntity users){
+        log.info("=> consumed{}", writeValueAsString(users));
+    }
+
+    private String writeValueAsString(UsersEntity users){
+        try {
+            return objectMapper.writeValueAsString(users);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
